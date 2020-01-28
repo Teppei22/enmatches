669a9f7758cb7313c4d3262c7e63000b9af9a922
Teppei FUJII <ft22.phyma@gmail.com>
2020-01-28 23:45:59 +0900

    homeをmypageに名称変更・リセットパスワードメール機能

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Work;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\MessageController;
 
-class HomeController extends Controller
+/**
+ * マイページでの処理
+ * 
+ */
+class HomeController extends MessageController
 {
     /**
      * Create a new controller instance.
@@ -13,7 +22,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+
     }
 
     /**
@@ -21,8 +30,23 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index($is_image = false)
     {
-        return view('home');
+
+
+        // ログインユーザに関連する案件を取得
+        $posted_works = Auth::user()->postedWorks()->with('postUser')->get();
+        $applied_works = Auth::user()->appliedWorks()->with('postUser')->get();
+        
+        // ログインユーザに関連するメッセージを取得
+        [
+            "posted_works" =>$posted_works,
+            "applied_works" =>$applied_works,
+        ]
+        = $this->getLatestMsgWorks($posted_works, $applied_works);
+
+        
+
+        return view('mypage',compact('posted_works','applied_works'));
     }
 }
---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CustomResetPassword extends Notification
+{
+    /**
+     * パスワードリセット
+     * 
+     * @var string 
+     * */
+    
+    public $token;
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject(__('Reset Password'))
+            ->line(__('Click button below and reset password.'))
+            ->action(__('Reset Password'), url('password/reset', $this->token))
+            ->line(__('If you did not request a password reset, no further action is required.'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
